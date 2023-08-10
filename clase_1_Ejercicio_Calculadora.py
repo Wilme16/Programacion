3392121074c4393d8a254e5a237ca0ad6cc21404
Wilme16 <u20202191178@usco.edu.co>
2023-08-09 20:42:39 -0500

    cambio

---
+++ clase_1_Ejercicio_Calculadora.py
@@ -0,0 +1,35 @@
+x = int(input("Ingrese un número: "))
+y = int(input("Ingrese otro número: "))
+
+print("Su primer número es:", x)
+print("Su segundo número es:", y)
+
+simbolo = input("Ingrese el símbolo de la operación que desea realizar (+, -, *, /, %): ")
+# -----------------------------------------------------------------------------------------------
+if simbolo == "+":
+    suma = x + y
+    print("La suma de los números es:", suma)
+# -----------------------------------------------------------------------------------------------
+elif simbolo == "-":
+    resta = x - y 
+    print("La resta de los números es:", resta)
+# -----------------------------------------------------------------------------------------------
+elif simbolo == "/":
+    if y != 0:
+        division = x / y
+        print("La división de los números es:", division)
+    else:
+        print("Error: No se puede dividir por cero.")
+# -----------------------------------------------------------------------------------------------      
+elif simbolo == "*":
+    multiplicacion = x * y
+    print("La multiplicación de los números es:", multiplicacion)
+# ----------------------------------------------------------------------------------------------- 
+elif simbolo == "%":
+    if y != 0:
+        modulo = x % y 
+        print("El módulo de la división es:", modulo)
+    else:
+        print("Error: No se puede calcular el módulo con cero.")
+else:
+    print("Operador no válido.")
